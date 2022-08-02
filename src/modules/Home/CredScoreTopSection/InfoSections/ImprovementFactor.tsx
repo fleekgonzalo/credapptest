eb62a6f09d0367a747de3a567817cc6fca2e751a
Khang Nguyen <nguyenak95@gmail.com>
2022-08-02 20:08:16 +0700

    add filter for improvement factor (#36)

--- src/modules/Home/CredScoreTopSection/InfoSections/ImprovementFactor.tsx
+++ src/modules/Home/CredScoreTopSection/InfoSections/ImprovementFactor.tsx
@@ -4,14 +4,33 @@ import { ChevronRightIcon } from "@/common/components/CustomIcon";
 import { APIResultContext } from "@/common/context/api.context";
 import InfoCard from "@/modules/Home/CredScoreTopSection/InfoSections/InfoCard";
 
+const IMPROVEMENT_FACTOR_INDEX = 6;
+const filterText =
+  "Unfortunately, we currently don't have enough data to suggest credit scoring feedback for this wallet.";
 const ImprovementFactor = () => {
   const {
     reportAddress: { data },
   } = useContext(APIResultContext);
+
+  if (data && data.report.factors.length < 7) {
+    return (
+      <InfoCard headingText="improvement factor">
+        <div className="flex gap-x-2">
+          <img
+            alt="no token data"
+            className="inline w-10 h-10"
+            src="/image/no_data_circle.png"
+          />
+          <p className="tracking-[0.02em] font-normal">{filterText}</p>
+        </div>
+      </InfoCard>
+    );
+  }
+
   return (
     <InfoCard headingText="improvement factor">
       <p className="text-base">
-        {data?.report.factors[data.report.factors.length - 1].description ||
+        {data?.report.factors[IMPROVEMENT_FACTOR_INDEX].description ||
           "No data"}
       </p>
 
