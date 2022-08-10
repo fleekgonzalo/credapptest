b46d6a18000d4b90ed8f73ea4bc9782124545d83
Khang Nguyen <nguyenak95@gmail.com>
2022-08-10 21:00:16 +0700

    fix bug dropdown doesn't work piechart (#47)

--- src/common/components/Dropdown/Dropdown.tsx
+++ src/common/components/Dropdown/Dropdown.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React, { HTMLAttributes, useState } from "react";
+import React, { HTMLAttributes, useEffect, useRef, useState } from "react";
 
 import { ChevronDownIcon } from "../CustomIcon";
 
@@ -20,26 +20,43 @@ export const Dropdown = ({
 }: DropdownProps) => {
   const [active, setActive] = useState(false);
   const [selected, setSelected] = useState(value || options[0].value);
+  const ref = useRef(null);
 
   const handleClickOption = (item: DropDownOptions) => (e) => {
-    e.preventDefault();
+    e.stopPropagation();
     setSelected(item.value);
     onChange(item);
     setActive(false);
   };
+  const handleOpenDropdown = () => {
+    setActive(true);
+  };
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        setActive(false);
+      }
+    };
+
+    document.addEventListener("click", handler);
+
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
+
   return (
     <div
-      className={classNames("relative", className)}
-      onBlur={() => setActive(false)}
-      onFocus={() => setActive(true)}
-      onMouseOut={() => setActive(false)}
-      onMouseOver={() => setActive(true)}
+      ref={ref}
+      className={classNames("relative dropdown-button", className)}
+      onClick={handleOpenDropdown}
     >
       <div>
         <button
           aria-expanded="true"
           aria-haspopup="true"
-          className="inline-flex relative min-w-[100px] gap-x-4 justify-start rounded-[4px] border border-cred-dark-gray px-2 py-1 pr-3 shadow-sm text-sm font-normal leading-[22px] focus:outline-none focus:ring-2 focus:ring-offset-2"
+          className="inline-flex relative min-w-[100px] gap-x-4 justify-start rounded-[4px] border border-cred-dark-gray px-2 py-1 pr-3 shadow-sm text-sm font-normal leading-[22px] "
           id="menu-button"
           type="button"
         >
