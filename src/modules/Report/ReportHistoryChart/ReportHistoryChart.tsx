b0e59d998a7cb2c47a8bf60de9d51e0656dfa549
Khang Nguyen <nguyenak95@gmail.com>
2022-08-17 21:52:13 +0700

    add extra space to tooltip (#57)

--- src/modules/Report/ReportHistoryChart/ReportHistoryChart.tsx
+++ src/modules/Report/ReportHistoryChart/ReportHistoryChart.tsx
@@ -57,7 +57,10 @@ export const ReportHistoryChart = ({
             Your account history
             <InfoWithTooltip>
               <div className="w-[209px] text-xs">
-                {"Aggregated metrics of holding in the last 5 months ($USD)."}
+                <p>
+                  Aggregated metrics of holding in the last 5 months ($USD).
+                </p>
+                <br />
                 <p>
                   Months with missing dots indicate missing data for account.
                 </p>
