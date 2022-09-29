1898b982c4fccf0212376515614eef9497c10b4d
Julian Gay <julian@xen.do>
2022-09-28 20:32:59 -0700

    Minor formatting and wording updates

--- src/common/components/SubscribeSection/SubscribeSection.tsx
+++ src/common/components/SubscribeSection/SubscribeSection.tsx
@@ -4,7 +4,7 @@ import { Button } from "../Button";
 
 export const SubscribeSection = () => {
   return (
-    <div className="relative mt-6 bg-[url('/image/subscribe_background.png')] h-[72px] bg-cover mix-blend-lighten border-2 border-[#5d78ff33] rounded-xl">
+    <div className="relative mt-8 bg-[url('/image/subscribe_background.png')] h-[72px] bg-cover mix-blend-lighten border-2 border-[#5d78ff33] rounded-xl">
       <div className="w-full h-full bg-cred-soft-blue opacity-20 rounded-xl"></div>
       <div className="absolute top-0 flex items-center justify-center w-full h-full gap-x-5">
         <h1 className="text-[24px]">
